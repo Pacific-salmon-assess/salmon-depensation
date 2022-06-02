ceeb414b917ca6ec37a5a1ef6315a2cbe1efcc1a
Dan Greenberg <dan.greenberg01@gmail.com>
2022-06-02 14:46:39 -0700

    start of batched model code

--- code/batched_model_runs.R
+++ code/batched_model_runs.R
@@ -1,10 +1,10 @@
-#Preliminary batched model support for varying dynamics in sockeye
+#Batched model for all salmon stocks
 library(here);library(dplyr)
-sock_dat<- read.csv(here('data','filtered datasets','sockeye_final.csv'))
-sock_info<- read.csv(here('data','filtered datasets','filtered_sockeye_info.csv'))
+stock_dat<- read.csv(here('data','salmon_productivity_compilation_jun2022.csv'))
+stock_info<- read.csv(here('data','all_stocks_info_jun2022.csv'))
 library(cmdstanr);library(loo);library(dlm)
 
-source(here('code','plot_functions.R'))
+source(here('code','functions.R'))
 source(here('code','dlm-wrapper.R'))
 
 sock_info<- subset(sock_info, stock.id %in% sock_dat$stock.id)
@@ -108,20 +108,20 @@ for(i in 1:nrow(sock_info)){
   avary<-fitDLM(SRdata, alpha_vary = TRUE, beta_vary = FALSE)
   
   
- # fit2test<- mtest$sample(
- #   data = data,
-#    init=0,
-#    seed = 123, 
- #   chains = 6, 
- #   parallel_chains = 6,
- #   iter_warmup = 500,
- #   iter_sampling = 1000,
-#    refresh = 500,
- #   adapt_delta = 0.99,
- #   max_treedepth = 20 # print update every 500 iters
- # )
+  # fit2test<- mtest$sample(
+  #   data = data,
+  #    init=0,
+  #    seed = 123, 
+  #   chains = 6, 
+  #   parallel_chains = 6,
+  #   iter_warmup = 500,
+  #   iter_sampling = 1000,
+  #    refresh = 500,
+  #   adapt_delta = 0.99,
+  #   max_treedepth = 20 # print update every 500 iters
+  # )
   
- # params2test<- fit2test$draws(format='df',variables=c('log_a','b','log_b','sigma_a','sigma_e','sigma_v'))
+  # params2test<- fit2test$draws(format='df',variables=c('log_a','b','log_b','sigma_a','sigma_e','sigma_v'))
   #fit2test$summary(variables=c('sigma_a','sigma_e','sigma_v'))
   
   #TV capacity
@@ -140,17 +140,17 @@ for(i in 1:nrow(sock_info)){
   
   bvary<-fitDLM(SRdata, alpha_vary = FALSE, beta_vary = TRUE)
   
- # fit3_test<- mod3_t$sample(
+  # fit3_test<- mod3_t$sample(
   #  data = data,
   #  seed = 123, 
-   # init=0,
+  # init=0,
   #chains = 6, 
-   # parallel_chains = 6,
-    #iter_warmup = 500,
-    #iter_sampling = 1000,
-    #refresh = 500,
-    #adapt_delta = 0.99,
-    #max_treedepth = 20 # print update every 500 iters
+  # parallel_chains = 6,
+  #iter_warmup = 500,
+  #iter_sampling = 1000,
+  #refresh = 500,
+  #adapt_delta = 0.99,
+  #max_treedepth = 20 # print update every 500 iters
   #)
   
   #TV capacity - GP
@@ -182,18 +182,18 @@ for(i in 1:nrow(sock_info)){
   )
   
   #correlated walks with a & b
-#  fit4.2<- mod4.2$sample(
-#    data = data,
-#    init=0,
- #   seed = 123, 
-#    chains = 6, 
-#    parallel_chains = 6,
- #   iter_warmup = 500,
- #   iter_sampling = 1000,
- #   refresh = 500,
+  #  fit4.2<- mod4.2$sample(
+  #    data = data,
+  #    init=0,
+  #   seed = 123, 
+  #    chains = 6, 
+  #    parallel_chains = 6,
+  #   iter_warmup = 500,
+  #   iter_sampling = 1000,
+  #   refresh = 500,
   #  adapt_delta = 0.99,
   #  max_treedepth = 20 # print update every 500 iters
- # )
+  # )
   #TV productivity & capacity - GP
   fit4gp<- mod4gp$sample(
     data = data,
@@ -210,62 +210,62 @@ for(i in 1:nrow(sock_info)){
   
   abvary<-fitDLM(SRdata, alpha_vary = TRUE, beta_vary = TRUE)
   
- # elpd1= fit1$loo(cores=2)
-#  elpd2= fit2$loo(cores=2)
- # elpd2gp= fit2gp$loo(cores=2)
-#  elpd3= fit3$loo(cores=2)
- # elpd3gp= fit3gp$loo(cores=2)
-#  elpd4= fit4$loo(cores=2)
- # elpd4gp= fit4gp$loo(cores=2)
+  # elpd1= fit1$loo(cores=2)
+  #  elpd2= fit2$loo(cores=2)
+  # elpd2gp= fit2gp$loo(cores=2)
+  #  elpd3= fit3$loo(cores=2)
+  # elpd3gp= fit3gp$loo(cores=2)
+  #  elpd4= fit4$loo(cores=2)
+  # elpd4gp= fit4gp$loo(cores=2)
   #elpd_comp<- loo::loo_compare(elpd1,elpd2,elpd2gp,elpd3,elpd3gp,elpd4,elpd4gp)
   
- # lpd_point <- cbind(
-#    elpd1$pointwise[,"elpd_loo"],
-#    elpd2$pointwise[,"elpd_loo"],
- #   elpd2gp$pointwise[,"elpd_loo"],
-#    elpd3$pointwise[,"elpd_loo"],
- #   elpd3gp$pointwise[,"elpd_loo"],
-#    elpd4$pointwise[,"elpd_loo"],
- #   elpd4gp$pointwise[,"elpd_loo"]
-#  )
-#  mod_weights<-stacking_weights(lpd_point)
+  # lpd_point <- cbind(
+  #    elpd1$pointwise[,"elpd_loo"],
+  #    elpd2$pointwise[,"elpd_loo"],
+  #   elpd2gp$pointwise[,"elpd_loo"],
+  #    elpd3$pointwise[,"elpd_loo"],
+  #   elpd3gp$pointwise[,"elpd_loo"],
+  #    elpd4$pointwise[,"elpd_loo"],
+  #   elpd4gp$pointwise[,"elpd_loo"]
+  #  )
+  #  mod_weights<-stacking_weights(lpd_point)
   
- #  weights=stacking_weights(lpd_point)
-#   sock_info$w1[i]=weights[1]
-#   sock_info$w2[i]=weights[2]
-#   sock_info$w2gp[i]=weights[3]
- #  sock_info$w3[i]=weights[4]
-#   sock_info$w3gp[i]=weights[5]
-#  sock_info$w4[i]=weights[6]
- #  sock_info$w4gp[i]=weights[7]
-   
-   params1<- fit1$draws(format='df',variables=c('log_a','b','log_b'))
-   params2<- fit2$draws(format='df',variables=c('log_a','b','log_b'))
-   params2gp<- fit2gp$draws(format='df',variables=c('log_a','b','log_b'))
-   params2ou<- fit2ou$draws(format='df',variables=c('log_a'))
-   params3<- fit3$draws(format='df',variables=c('log_a','b','log_b'))
-   params3gp<- fit3gp$draws(format='df',variables=c('log_a','b','log_b'))
-   params4<- fit4$draws(format='df',variables=c('log_a','b','log_b'))
-   params4.2<- fit4.2$draws(format='df',variables=c('log_a','b','log_b'))
-   params4gp<- fit4gp$draws(format='df',variables=c('log_a','b','log_b'))
-   
-   pars_mod1=c('log_a','b','log_b','sigma_e')
-   pars_mod2=c('log_a','b','log_b','sigma_e','sigma_a')
-   pars_mod2gp=c('log_a','b','log_b','sigma_e','gp_tau','gp_rho')
-   pars_mod3=c('log_a','b','log_b','sigma_e','sigma_b')
-   pars_mod4=c('log_a','b','log_b','sigma_e','sigma_a','sigma_b')
-   pars_mod4gp=c('log_a','b','log_b','sigma_e','gp_tau_a','gp_rho_a','gp_tau_b','gp_rho_b')
-   
- #  mod_par_path_1<- here('outputs','initial stan runs','sockeye','1 - static')
-#   mod_par_path_2<- here('outputs','initial stan runs','sockeye','2 - a')
- #  mod_par_path_3<- here('outputs','initial stan runs','sockeye','3 - b')
+  #  weights=stacking_weights(lpd_point)
+  #   sock_info$w1[i]=weights[1]
+  #   sock_info$w2[i]=weights[2]
+  #   sock_info$w2gp[i]=weights[3]
+  #  sock_info$w3[i]=weights[4]
+  #   sock_info$w3gp[i]=weights[5]
+  #  sock_info$w4[i]=weights[6]
+  #  sock_info$w4gp[i]=weights[7]
+  
+  params1<- fit1$draws(format='df',variables=c('log_a','b','log_b'))
+  params2<- fit2$draws(format='df',variables=c('log_a','b','log_b'))
+  params2gp<- fit2gp$draws(format='df',variables=c('log_a','b','log_b'))
+  params2ou<- fit2ou$draws(format='df',variables=c('log_a'))
+  params3<- fit3$draws(format='df',variables=c('log_a','b','log_b'))
+  params3gp<- fit3gp$draws(format='df',variables=c('log_a','b','log_b'))
+  params4<- fit4$draws(format='df',variables=c('log_a','b','log_b'))
+  params4.2<- fit4.2$draws(format='df',variables=c('log_a','b','log_b'))
+  params4gp<- fit4gp$draws(format='df',variables=c('log_a','b','log_b'))
+  
+  pars_mod1=c('log_a','b','log_b','sigma_e')
+  pars_mod2=c('log_a','b','log_b','sigma_e','sigma_a')
+  pars_mod2gp=c('log_a','b','log_b','sigma_e','gp_tau','gp_rho')
+  pars_mod3=c('log_a','b','log_b','sigma_e','sigma_b')
+  pars_mod4=c('log_a','b','log_b','sigma_e','sigma_a','sigma_b')
+  pars_mod4gp=c('log_a','b','log_b','sigma_e','gp_tau_a','gp_rho_a','gp_tau_b','gp_rho_b')
+  
+  #  mod_par_path_1<- here('outputs','initial stan runs','sockeye','1 - static')
+  #   mod_par_path_2<- here('outputs','initial stan runs','sockeye','2 - a')
+  #  mod_par_path_3<- here('outputs','initial stan runs','sockeye','3 - b')
   # mod_par_path_4<- here('outputs','initial stan runs','sockeye','4 - a and b')
-   
+  
   # write.csv(fit1$draws(format='df',variables=pars_mod1),file.path(mod_par_path_1,paste(sprintf("%02d",i),'_',sock_info$Stock[i],sock_info$Species[i],'_model1','.csv',sep='')))
   # write.csv(fit2$draws(format='df',variables=pars_mod1),file.path(mod_par_path_2,paste(sprintf("%02d",i),'_',sock_info$Stock[i],sock_info$Species[i],'_model2','.csv',sep='')))
   # write.csv(fit3$draws(format='df',variables=pars_mod1),file.path(mod_par_path_3,paste(sprintf("%02d",i),'_',sock_info$Stock[i],sock_info$Species[i],'_model3','.csv',sep='')))
-   #write.csv(fit4$draws(format='df',variables=pars_mod1),file.path(mod_par_path_4,paste(sprintf("%02d",i),'_',sock_info$Stock[i],sock_info$Species[i],'_model4','.csv',sep='')))
-   
+  #write.csv(fit4$draws(format='df',variables=pars_mod1),file.path(mod_par_path_4,paste(sprintf("%02d",i),'_',sock_info$Stock[i],sock_info$Species[i],'_model4','.csv',sep='')))
+  
   # mod_par_path_sum<- here('outputs','initial stan runs','sockeye','model summaries')
   # write.csv(as.data.frame(fit1$summary(variables=pars_mod1)),file.path(mod_par_path_sum,paste(sprintf("%02d",i),'_',s$stock[i],s$species[i],'_model1_summary','.csv',sep='')))
   # write.csv(as.data.frame(fit2$summary(variables=pars_mod2)),file.path(mod_par_path_sum,paste(sprintf("%02d",i),'_',s$stock[i],s$species[i],'_model2_summary','.csv',sep='')))
@@ -274,11 +274,11 @@ for(i in 1:nrow(sock_info)){
   # write.csv(as.data.frame(fit3gp$summary(variables=pars_mod2gp)),file.path(mod_par_path_sum,paste(sprintf("%02d",i),'_',s$stock[i],s$species[i],'_model3gp_summary','.csv',sep='')))
   # write.csv(as.data.frame(fit4$summary(variables=pars_mod4)),file.path(mod_par_path_sum,paste(sprintf("%02d",i),'_',s$stock[i],s$species[i],'_model4_summary','.csv',sep='')))
   # write.csv(as.data.frame(fit4gp$summary(variables=pars_mod4gp)),file.path(mod_par_path_sum,paste(sprintf("%02d",i),'_',s$stock[i],s$species[i],'_model4gp_summary','.csv',sep='')))
-   
-   rw_gp_dlm_ou_plot_comp(params=params2,params_gp = params2gp,params_dlm=avary$results,type=1,x=s,pdf=1)
-   rw_gp_dlm_plot_comp(params=params2,params_gp = params2gp,params_dlm=avary$results,type=1,x=s,pdf=1)
-   rw_gp_dlm_plot_comp(params=params3,params_gp = params3gp,params_dlm=bvary$results,type=2,x=s,pdf=1)
-   rw_gp_dlm_plot_comp(params=params4,params_gp = params4gp,params_dlm=abvary$results,type=3,x=s,pdf=1)
+  
+  rw_gp_dlm_ou_plot_comp(params=params2,params_gp = params2gp,params_dlm=avary$results,type=1,x=s,pdf=1)
+  rw_gp_dlm_plot_comp(params=params2,params_gp = params2gp,params_dlm=avary$results,type=1,x=s,pdf=1)
+  rw_gp_dlm_plot_comp(params=params3,params_gp = params3gp,params_dlm=bvary$results,type=2,x=s,pdf=1)
+  rw_gp_dlm_plot_comp(params=params4,params_gp = params4gp,params_dlm=abvary$results,type=3,x=s,pdf=1)
 }
 
 
@@ -340,7 +340,7 @@ for(i in 1:nrow(sock_info)){
   params2ou<- fit2ou$draws(format='df',variables=c('log_a'))
   
   rw_gp_dlm_ou_plot_comp(params=params2,params_gp = params2gp,params_dlm=avary$results,params_ou=params2ou,x=s,pdf=0)
-
+  
 }
 
 #correlated vs uncorrelated random walks
@@ -370,28 +370,28 @@ for(i in 2:nrow(sock_info)){
     max_treedepth = 20 # print update every 500 iters
   )
   
-#  correlated walks with a & b
-    fit4.2<- mod4.2$sample(
-      data = data,
-      init=0,
-     seed = 123, 
-      chains = 6, 
-      parallel_chains = 6,
-     iter_warmup = 500,
-     iter_sampling = 1000,
-     refresh = 500,
+  #  correlated walks with a & b
+  fit4.2<- mod4.2$sample(
+    data = data,
+    init=0,
+    seed = 123, 
+    chains = 6, 
+    parallel_chains = 6,
+    iter_warmup = 500,
+    iter_sampling = 1000,
+    refresh = 500,
     adapt_delta = 0.99,
     max_treedepth = 20 # print update every 500 iters
-   )
+  )
   
   elpd4= fit4$loo(cores=2)
   elpd4.2= fit4.2$loo(cores=2)
   elpd_comp<- loo::loo_compare(elpd4,elpd4.2)
   
   lpd_point <- cbind(
-      elpd4$pointwise[,"elpd_loo"],
-     elpd4.2$pointwise[,"elpd_loo"]
-    )
+    elpd4$pointwise[,"elpd_loo"],
+    elpd4.2$pointwise[,"elpd_loo"]
+  )
   weights=stacking_weights(lpd_point)
   
   summ_corr[i,2]=elpd4$estimates[1,1]
@@ -401,7 +401,7 @@ for(i in 2:nrow(sock_info)){
   summ_corr[i,6]=weights[1]
   summ_corr[i,7]=weights[2]
   
- 
+  
   params4<- fit4$draws(format='df',variables=c('log_a','b','log_b'))
   params4.2<- fit4.2$draws(format='df',variables=c('log_a','b','log_b','Cor_1'))
   
@@ -419,7 +419,7 @@ summary(factor(sock_info$mod_sec))
 for(i in 1:nrow(sock_info)){
   if(sock_info$mod_sec[i]==1){
     params=read.csv(here('outputs','initial stan runs','sockeye','1 - static',paste(sprintf("%02d",i),'_',sock_info$Stock[i],sock_info$Species[i],'_model1','.csv',sep='')))
-  
+    
     log_a= params[,(gsub('\\..*','',colnames(params))=='log_a')]
     b= params[,(gsub('\\..*','',colnames(params))=='b')]
     a.med=median(log_a)
@@ -429,10 +429,10 @@ for(i in 1:nrow(sock_info)){
     b.med=median(b)
     b.l80=quantile(b,0.1)
     b.u80=quantile(b,0.9)
-    }
+  }
   if(sock_info$mod_sec[i]==2){
     params=read.csv(here('outputs','initial stan runs','sockeye','2 - a',paste(sprintf("%02d",i),'_',sock_info$Stock[i],sock_info$Species[i],'_model2','.csv',sep='')))
-  
+    
     log_a= params[,(gsub('\\..*','',colnames(params))=='log_a')]
     b= params[,(gsub('\\..*','',colnames(params))=='b')]
     a.med=apply(log_a,2,median)
@@ -443,10 +443,10 @@ for(i in 1:nrow(sock_info)){
     b.l80=quantile(b,0.1)
     b.u80=quantile(b,0.9)
     
-    }
+  }
   if(sock_info$mod_sec[i]==3){
     params=read.csv(here('outputs','initial stan runs','sockeye','3 - b',paste(sprintf("%02d",i),'_',sock_info$Stock[i],sock_info$Species[i],'_model3','.csv',sep='')))
-  
+    
     log_a= params[,(gsub('\\..*','',colnames(params))=='log_a')]
     b= params[,(gsub('\\..*','',colnames(params))=='b')]
     a.med=median(log_a)
@@ -458,10 +458,10 @@ for(i in 1:nrow(sock_info)){
     b.u80=apply(b,2, quantile, probs=c(0.9))
     
     
-    }
+  }
   if(sock_info$mod_sec[i]==4){
     params=read.csv(here('outputs','initial stan runs','sockeye','4 - a and b',paste(sprintf("%02d",i),'_',sock_info$Stock[i],sock_info$Species[i],'_model4','.csv',sep='')))
-  
+    
     log_a= params[,(gsub('\\..*','',colnames(params))=='log_a')]
     b= params[,(gsub('\\..*','',colnames(params))=='b')]
     a.med=apply(log_a,2,median)
@@ -471,8 +471,8 @@ for(i in 1:nrow(sock_info)){
     b.med=apply(b,2,median)
     b.l80=apply(b,2, quantile, probs=c(0.1))
     b.u80=apply(b,2, quantile, probs=c(0.9))
-    }
- 
+  }
+  
   
   pdf(file.path(here('outputs','initial stan runs','sockeye','plots'),paste('Best fit',sock_info$Stock[i],sock_info$Species[i],sep='_','.pdf')),width=14,height=8.5)
   par(mfrow=c(1,2))
@@ -481,7 +481,7 @@ for(i in 1:nrow(sock_info)){
   plot(b.med~seq(sock_info$ts.start[i],sock_info$ts.end[i]),type='l',ylim=c(min(b.l80),max(b.u80)),ylab='',main=paste('Capacity -',sock_info$Stock[i],sock_info$Species[i],sep=' '))
   lines(b.l80,lty=5);lines(b.u80,lty=5)
   dev.off()
-
+  
 }
 
 
